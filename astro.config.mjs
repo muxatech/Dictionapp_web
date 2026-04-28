4621a3ea16552f19cc04d90dab4e5cef71bd7e21
muxatech <dictionapp.muxatech@gmail.com>
2026-04-28 20:24:41 +0200

    Mejora landing: nuevas secciones, páginas academias/contacto/términos, SEO y sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://dictionapp.com',
   integrations: [
     tailwind({
       applyBaseStyles: true,
     }),
+    sitemap(),
   ],
 });
